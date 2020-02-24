5499f278924fe88acc2c5752594ed48b1d9eb05e
Rae Glazier <55262966+raegdev@users.noreply.github.com>
2020-02-24 11:51:55 -0600

    Revert "completed mvp"

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,18 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-import Home from './Home';
-import Contact from './Contact';
-import About from './About';
-
 const Navigation = () => {
   return (
     <div>
-
-      
-
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <Link to = '/' >Home</Link>
+          <a href="">Home</a>
         </div>
         <div>
-          <Link to = '/about' >About</Link>
+          <a href="">About</a>
         </div>
         <div>
-          <Link to = '/contact' >Contact</Link>
+          <a href="">Contact</a>
         </div>
       </div>
     </div>
